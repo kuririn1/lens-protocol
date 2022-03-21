0778d2e19e19bdb3935fe3fabe8e9e1a47675766
Kuririn <kuririn1@gmail.com>
2022-03-21 17:16:47 +0100

    basic module functionality (2)

--- contracts/core/modules/collect/VoteCollectModule.sol
+++ contracts/core/modules/collect/VoteCollectModule.sol
@@ -5,15 +5,33 @@ pragma solidity 0.8.10;
 import {ICollectModule} from '../../../interfaces/ICollectModule.sol';
 import {ModuleBase} from '../ModuleBase.sol';
 
+struct Vote {
+    uint8 voteValue;
+    uint256 timestamp;
+}
+
+struct ProfilePublicationData {
+    mapping(address => Vote) voteByCollector;
+    uint8 maxVoteNum;
+}
+
 contract VoteCollectModule is ICollectModule, ModuleBase {
+    error VoteOverMax();
+    error AlreadyVoted();
+
+    mapping(uint256 => mapping(uint256 => ProfilePublicationData))
+        internal _dataByPublicationByProfile;
+
     constructor(address hub) ModuleBase(hub) {}
 
     function initializePublicationCollectModule(
         uint256 profileId,
         uint256 pubId,
         bytes calldata data
-    ) external pure override returns (bytes memory) {
-        return new bytes(0);
+    ) external override onlyHub returns (bytes memory) {
+        uint8 maxVoteNum = abi.decode(data, (uint8));
+        _dataByPublicationByProfile[profileId][pubId].maxVoteNum = maxVoteNum;
+        return data;
     }
 
     function processCollect(
@@ -22,7 +40,18 @@ contract VoteCollectModule is ICollectModule, ModuleBase {
         uint256 profileId,
         uint256 pubId,
         bytes calldata data
-    ) external view override {
-        
+    ) external override onlyHub {
+        (uint8 voteValue) = abi.decode(data, (uint8));
+
+        if (voteValue > _dataByPublicationByProfile[profileId][pubId].maxVoteNum) {
+            revert VoteOverMax();
+        }
+
+        if(_dataByPublicationByProfile[profileId][pubId].voteByCollector[collector].timestamp > 0) {
+            revert AlreadyVoted();
+        } 
+
+        _dataByPublicationByProfile[profileId][pubId].voteByCollector[collector].voteValue = voteValue;
+        _dataByPublicationByProfile[profileId][pubId].voteByCollector[collector].timestamp = block.timestamp;
     }
 }
--- tasks/vote-collect.ts
+++ tasks/vote-collect.ts
@@ -1,5 +1,5 @@
 import { task } from 'hardhat/config';
-import { LensHub__factory } from '../typechain-types';
+import { LensHub__factory, VoteCollectModule__factory } from '../typechain-types';
 import { waitForTx, initEnv, getAddrs, deployContract } from './helpers/utils';
 
 task('vote-collect').setAction(async ({}, hre) => {
@@ -7,9 +7,9 @@ task('vote-collect').setAction(async ({}, hre) => {
     const addrs = getAddrs();
     const lensHub = LensHub__factory.connect(addrs['lensHub proxy'], governance);
 
-   // const voteCollectModule = await deployContract(
-      //  new VoteCollectModule__factory(governance).deploy(lensHub.address)
-  //    );
+    const voteCollectModule = await deployContract(
+        new VoteCollectModule__factory(governance).deploy(lensHub.address)
+      );
 
-   // await waitForTx(lensHub.whitelistCollectModule(voteCollectModule.address, true));  
+    await waitForTx(lensHub.whitelistCollectModule(voteCollectModule.address, true));  
 });
