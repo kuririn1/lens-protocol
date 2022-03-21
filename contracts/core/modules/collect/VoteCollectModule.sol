061cc877c9b042b2aa4728c7e96f9fffa8a1a4ee
Kuririn <kuririn1@gmail.com>
2022-03-21 16:41:03 +0100

    add voteCollectModule - empty

---
+++ contracts/core/modules/collect/VoteCollectModule.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+pragma solidity 0.8.10;
+
+import {ICollectModule} from '../../../interfaces/ICollectModule.sol';
+import {ModuleBase} from '../ModuleBase.sol';
+
+contract VoteCollectModule is ICollectModule, ModuleBase {
+    constructor(address hub) ModuleBase(hub) {}
+
+    function initializePublicationCollectModule(
+        uint256 profileId,
+        uint256 pubId,
+        bytes calldata data
+    ) external pure override returns (bytes memory) {
+        return new bytes(0);
+    }
+
+    function processCollect(
+        uint256 referrerProfileId,
+        address collector,
+        uint256 profileId,
+        uint256 pubId,
+        bytes calldata data
+    ) external view override {
+        
+    }
+}
